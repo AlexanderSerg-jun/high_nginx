a276bf4b7cff34ee3e3e3e6dbef5711883bc568a
Alexander <itspecos@gmail.com>
2024-08-13 17:36:09 +0300

    change_main

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    yandex={
+        source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">=0.13"
+}
 provider "yandex" {
+    token = var.access["token"]
+    cloud_id = var.access["cloud_id"]
+    folder_id = var.access["folder_id"]
+    zone = var.access["zone"]
+  
+}
+resource "yandex_compute_instance" "" {
   
 }
