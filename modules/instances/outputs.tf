d43f3d3cf57eee5b05042ef8a99cf5e1efa121af
AlexanderSerg-jun <85576634+AlexanderSerg-jun@users.noreply.github.com>
2024-08-16 12:52:20 +0300

    add *tf file in instance

--- modules/instances/outputs.tf
+++ modules/instances/outputs.tf
@@ -3,9 +3,9 @@ output "vm_name" {
 }
 
 output "instance_external_ip_address" {
-  value = yandex_compute_instance.instances.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.nodes.network_interface.0.nat_ip_address
 }
 
 output "instance_internal_ip_address" {
-  value = yandex_compute_instance.instances.network_interface.0.ip_address
+  value = yandex_compute_instance.nodes.network_interface.0.ip_address
 }
---
+++ modules/instances/providers.tf
@@ -0,0 +1,8 @@
+// описание провайдера
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
