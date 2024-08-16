f08027c700314b6a9f45c24c9d4dcdf2b3172538
AlexanderSerg-jun <85576634+AlexanderSerg-jun@users.noreply.github.com>
2024-08-16 12:19:37 +0300

    clear

---
+++ main.tf
@@ -0,0 +1,26 @@
+//описание локальный файлов и пользователя виртуальной машины
+locals {
+    //пользователь виртуальной машины
+    vm_user= alex
+    //ssh пользователя  публичный и часный ключ для подключения
+    ssh_public_key = ""
+    ssh_private_key = ""
+    //виртуальная частная сеть в облаке
+    vpc_name = "my_network"
+    // виртуальная частная подсеть имя и сетевая адресация
+    subnet_cidrs = ["172.18.0.0/24"]
+    subnet_name ="my_subnet"
+}
+// ресурс yandex cloud  виртаульная сеть
+resource "yandex_vpc_network" "vnc"{
+    // имя сети использует значение из locals
+    name = local.vpc_name
+}
+// ресурс yandex cloud виртуальная подсеть
+resource "yandex_vpc_subnet" "vsc"{
+    name = local.subnet_name
+    zone = local.zone
+    network_id = yandex_vpc_network.vnc.id
+    v4_cidr_blocks = local.subnet_cidrs
+}
+// описание
